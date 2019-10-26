f6f89fe0f27b1fbc3f4c7b4b431cd47ca5ec06b3
benjaminheine <benjaminheine@gmx.net>
2019-10-26 14:50:49 +0200

    Calculation is working, display of digits needs to be adapted and that the calculation is shown in the diplay after pressing the operator button

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
-//create an array for the calculation which stores numbers and operators
 let calculation = [];
 
-//functions for pressing numbers which shows numbers in the display and puts it to the array
-document.getElementById("seven").addEventListener("click", showNumberSeven);
-function showNumberSeven(e) {
-  e.preventDefault();
-  let number = e.target.childNodes["0"].nodeValue;
-  document.getElementById("digitField").value = number;
-  calculation.push(number);
+//Create event listener for buttons when page is loaded
+window.onload = function() {
+  createEventListenerForButtons();
+  createEventListenerForOperators();
+};
+
+//get all buttons and operators
+const buttons = document.querySelectorAll("button.numbers");
+const operators = document.querySelectorAll("button.operators");
+
+//create event listener for clicked button and push digit to an array and put digit to the display
+function createEventListenerForButtons() {
+  for (i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(e) {
+      e.preventDefault();
+      const number = e.target.childNodes[0].nodeValue;
+      document.getElementById("digitField").value =
+        document.getElementById("digitField").value + number;
+      calculation.push(number);
+    });
+  }
 }
 
-document.getElementById("four").addEventListener("click", showNumberFour);
-function showNumberFour(e) {
-  e.preventDefault();
-  const number = e.target.childNodes[0].nodeValue;
-  document.getElementById("digitField").value = number;
-  calculation.push(number);
+//create event listener for clicked operator, push operator to array and empty display
+function createEventListenerForOperators() {
+  for (i = 0; i < operators.length; i++) {
+    operators[i].addEventListener("click", function(e) {
+      e.preventDefault();
+      const operator = e.target.childNodes[0].nodeValue;
+      calculation.push(operator);
+      document.getElementById("digitField").value = "";
+    });
+  }
 }
 
-//function to put operators to the array
-document.getElementById("adder").addEventListener("click", addNumbers);
-function addNumbers(e) {
-  calculation.push("+");
-}
-
-//function to calculate the numbers of the array
+// function to calculate the numbers of the array
 document.getElementById("resulter").addEventListener("click", showResult);
 function showResult(e) {
-  let result = eval(calculation[0] + calculation[1] + calculation[2]);
+  e.preventDefault();
+  console.log(calculation);
+  let result = 0;
+  result = eval(calculation.join(""));
   document.getElementById("digitField").value = result;
+}
+
+// function to delete display and array
+document.getElementById("resetButton").addEventListener("click", deleteDisplay);
+function deleteDisplay(e) {
+  calculation = [];
+  document.getElementById("digitField").value = "";
 }
